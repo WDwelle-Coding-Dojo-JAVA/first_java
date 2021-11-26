7f8796d55da3484b20072364c86800e749f27199
Will Dwelle <willdwelle@frontier.com>
2021-11-26 14:01:55 -0800

    initial commit

---
+++ Self.java
@@ -0,0 +1,7 @@
+public class Self{
+    public static void main(String[] args) {
+        System.out.println("My name is Will");
+        System.out.println("I am 23 years old");
+        System.out.println("My home town is Santa Clara, CA");
+    }
+}
